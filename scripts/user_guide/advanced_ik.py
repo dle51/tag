865f4472be4b734f4f433965c3d68e58b6a76b75
Dan Le <dle5@luc.edu>
2025-05-16 10:14:18 -0500

    Repository Update 3 - Fixed

--- scripts/user_guide/advanced_ik.py
+++ scripts/user_guide/advanced_ik.py
@@ -5,13 +5,14 @@ import numpy as np
 import genesis as gs
 
 ### init
-gs.init()
+gs.init(seed=0, precision='32', logging_level='debug')
 
 ### scene
 scene = gs.Scene(
     show_viewer = False,
     rigid_options=gs.options.RigidOptions(
-        enable_joint_limit = False,
+        enable_joint_limit=False,
+        enable_collision=False,
     ),
     vis_options = gs.options.VisOptions(
         show_world_frame = True,
@@ -24,58 +25,75 @@ scene = gs.Scene(
 )
 
 ### entities
-plane = scene.add_entity(
+scene.add_entity(
     gs.morphs.Plane(),
 )
 robot = scene.add_entity(
     gs.morphs.MJCF(file='xml/franka_emika_panda/panda.xml'),
 )
 
+### Target links for visualization
+target_left = scene.add_entity(
+    gs.morphs.Mesh(
+        file='meshes/axis.obj',
+        scale=0.1,
+    ),
+    surface=gs.surfaces.Default(color=(1, 0.5, 0.5, 1)),
+)
+target_right = scene.add_entity(
+    gs.morphs.Mesh(
+        file='meshes/axis.obj',
+        scale=0.1,
+    ),
+    surface=gs.surfaces.Default(color=(0.5, 1.0, 0.5, 1)),
+)
+
 ### camera
 cam = scene.add_camera(
-    res = (1280, 720),
-    pos = (5.25, 0.0, 3.75),
+    res = (640, 480),
+    pos = (3.5, 0.0, 2.5),
     lookat = (0, 0, 0.5),
-    fov = 60,
+    fov = 30,
     GUI = False,
 )
 
 ### build
-n_envs = 16
-scene.build(n_envs=n_envs, env_spacing=(1.0, 1.0))
+
+scene.build()
 
 ### target positions
 # orient the fingers downwards
-target_quat = np.tile(np.array([0, 1, 0, 0]), [n_envs, 1])
+target_quat = np.array([0, 1, 0, 0])
 # center of the circle to be traced
-center = np.tile(np.array([0.4, -0.2, 0.25]), [n_envs, 1])
-#radius of the circle 
+center = np.array([0.4, -0.2, 0.25])
+# radius of the circle 
 r = 0.1
-# random speeds for each environment
-angular_speed = np.random.uniform(-10, 10, n_envs)
 
-ee_link = robot.get_link('hand')
+# Links
+left_finger = robot.get_link('left_finger')
+right_finger = robot.get_link('right_finger')
 
 ### activate camera
 cam.start_recording()
 
-### movement
-for i in range(0, 1000):
-    target_pos = np.zeros([n_envs, 3])
-    target_pos[:, 0] = center[:, 0] + np.cos(i/360*np.pi*angular_speed) * r
-    target_pos[:, 1] = center[:, 1] + np.sin(i/360*np.pi*angular_speed) * r
-    target_pos[:, 2] = center[:, 2]
-    target_q = np.hstack([target_pos, target_quat])
+### Movement
+for i in range(0, 2000):
+    target_pos_left = center + np.array([np.cos(i/360*np.pi), np.sin(i/360*np.pi), 0]) * r
+    target_pos_right = target_pos_left + np.array([0.0, 0.03, 0])
 
-    q = robot.inverse_kinematics(
-        link     = ee_link,
-        pos      = target_pos,
-        quat     = target_quat,
-        rot_mask = [False, False, True], # for demo purpose: only restrict direction of z-axis
+    target_left.set_qpos(np.concatenate([target_pos_left, target_quat]))
+    target_right.set_qpos(np.concatenate([target_pos_right, target_quat]))
+    
+    q = robot.inverse_kinematics_multilink(
+        links    = [left_finger, right_finger],
+        poss     = [target_pos_left, target_pos_right],
+        quats    = [target_quat, target_quat],
+        rot_mask = [False, False, True], # only restrict direction of z-axis
     )
 
-    robot.set_qpos(q)
+    robot.set_dofs_position(q)
     scene.visualizer.update()
     cam.render()
 
-cam.stop_recording(save_to_filename="./mp4/user_guide/parallel_ik_video.mp4", fps=60)
+### Saving Video
+cam.stop_recording(save_to_filename="./mp4/user_guide/advanced_ik_video.mp4", fps=60)
